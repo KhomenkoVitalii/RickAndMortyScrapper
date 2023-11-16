6324df499d2ac7728a028b79b64067f3a00630b2
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-16 13:32:29 +0200

    feat: changed models

--- scrapper/models.py
+++ scrapper/models.py
@@ -3,15 +3,15 @@ from django.utils import timezone
 
 
 class Location(models.Model):
-    name = models.CharField(max_length=255, default="unknown")
-    type = models.CharField(max_length=255, default="unknown")
-    dimension = models.CharField(max_length=255, default="unknown")
+    name = models.CharField(max_length=255, default="unknown", unique=True)
+    type = models.CharField(max_length=255, default="unknown", blank=True)
+    dimension = models.CharField(max_length=255, default="unknown", blank=True)
     url = models.CharField(max_length=255)
     created = models.DateTimeField(default=timezone.now, editable=False)
 
 
 class Character(models.Model):
-    name = models.CharField(max_length=255, default="unknown")
+    name = models.CharField(max_length=255, default="unknown", unique=True)
     status = models.CharField(max_length=255, default="unknown")
     species = models.CharField(max_length=255, default="unknown")
     type = models.CharField(max_length=255, default="unknown")
@@ -35,9 +35,9 @@ class Origin(models.Model):
 
 
 class Episode(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=25)
     air_date = models.CharField(max_length=255)
-    episode = models.CharField(max_length=255)
+    episode = models.CharField(max_length=255, unique=True)
 
     characters = models.ManyToManyField('Character', related_name='episodes')
 
