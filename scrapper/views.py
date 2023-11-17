8f8ea002b6fde86b4f90a272d764523ab93b08a8
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-17 21:06:59 +0200

    feat: created ReadOnly API for Location, Character, Episode

--- scrapper/views.py
+++ scrapper/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from scrapper.models import Location, Character, Episode
+from scrapper.serializer import LocationSerializer, CharacterSerializer, EpisodeSerializer
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 # Create your views here.
+
+
+class LocationView(ReadOnlyModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+
+class CharacterView(ReadOnlyModelViewSet):
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
+
+
+class EpisodeView(ReadOnlyModelViewSet):
+    queryset = Episode.objects.all()
+    serializer_class = EpisodeSerializer
