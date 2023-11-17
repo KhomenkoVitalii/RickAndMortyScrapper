b3252399c4ad98817bf1ca72edac140c675fdcb9
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-17 20:07:39 +0200

    feat: created UserCard model

--- game/models.py
+++ game/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from scrapper.models import Character
 
 # Create your models here.
+
+
+class UserCard(models.Model):
+    user_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    character_id = models.ForeignKey(to=Character, on_delete=models.DO_NOTHING)
+    is_liked = models.BooleanField(default=False)
