e37ffda645e73d554953ada0c55152362bfee34d
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-17 15:03:33 +0200

    feat: implemented 'episode_spider'

--- scrap.py
+++ scrap.py
@@ -1,10 +1,11 @@
-from scrapper.spider.location_spider import LocationSpider
-from scrapper.spider.character_spider import CharacterSpider
+from scrapper.spider import location_spider, character_spider, episode_spider
+
 import requests
 
 # to run this script use:
 # python manage.py shell < scrap.py
 
 # Don't change order of starting requests
-LocationSpider().start_requests()
-CharacterSpider().start_requests()
+location_spider.LocationSpider().start_requests()
+character_spider.CharacterSpider().start_requests()
+episode_spider.EpisodeSpider().start_requests()
