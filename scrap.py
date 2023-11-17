32a94287c2a051a559a7b3449c07e87712d2f000
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-17 14:04:50 +0200

    feat: implemented new spider

--- scrap.py
+++ scrap.py
@@ -1,6 +1,10 @@
 from scrapper.spider.location_spider import LocationSpider
+from scrapper.spider.character_spider import CharacterSpider
+import requests
 
 # to run this script use:
 # python manage.py shell < scrap.py
 
+# Don't change order of starting requests
 LocationSpider().start_requests()
+CharacterSpider().start_requests()
