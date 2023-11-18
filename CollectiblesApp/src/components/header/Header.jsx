047aa30340222445d6e6f42072951ef37e381b19
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-18 20:50:25 +0200

    feat: finished with Header component

--- CollectiblesApp/src/components/header/Header.jsx
+++ CollectiblesApp/src/components/header/Header.jsx
@@ -1,6 +1,25 @@
+import { Link } from 'react-router-dom'
+import styles from './Header.module.scss'
+
 const Header = () => {
     return <>
-        <h5>Rick and Morty Collectibles!</h5>
+        <header>
+            <div className={styles.logo}>
+                <Link to='/'>
+                    <img src="/Logo.svg" />
+                    <div className={styles.logoText}>
+                        <p>Rich and Morty</p>
+                        <p>collectibles</p>
+                    </div>
+                </Link>
+            </div>
+            <div className={styles.links}>
+                <Link to='/'>Home</Link>
+                <Link to='/'>Episodes</Link>
+                <Link to='/'>Characters</Link>
+                <Link to='/'>My collections</Link>
+            </div>
+        </header>
     </>
 }
 
