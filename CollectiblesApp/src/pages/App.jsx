039d3b1475f74d40cf8d738d3ab441ae79f520f7
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-18 19:20:44 +0200

    feat: implemented animated background

--- CollectiblesApp/src/pages/App.jsx
+++ CollectiblesApp/src/pages/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Outlet } from 'react-router-dom';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
+import Starfield from '../components/background/Starfield';
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
       <Header />
       <Outlet />
       <Footer />
+      <Starfield
+        starCount={1000}
+        starColor={[255, 255, 255]}
+        speedFactor={0.05}
+        backgroundColor="black"
+      />
     </>
   )
 }
