f28a54fbbd720dfe0d9f39b676be9a0801d70494
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-19 00:19:59 +0200

    update Home.jsx

--- CollectiblesApp/src/pages/home/Home.jsx
+++ CollectiblesApp/src/pages/home/Home.jsx
@@ -1,7 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Card from '../../components/card/Card'
+import homeRequest from '../../api/home_request';
+import styles from './Home.module.scss'
+
 const Home = () => {
-    return <>
-        <h1>HOME PAGE!</h1>
-    </>
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await homeRequest();
+                const jsonData = await response.json();
+                setData(jsonData);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <div className={styles.body}>
+                {data && <Card message={data.characters[0]} />}
+                {data && <Card message={data.characters[1]} />}
+                {data && <Card message={data.characters[2]} />}
+                {data && <Card message={data.characters[3]} />}
+            </div>
+        </>
+    );
 }
 
 export default Home;
