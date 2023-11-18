c173b9c537baf1ff5b264a8c57baba5d9e8f771b
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-18 21:49:31 +0200

    feat: simple footer added

--- CollectiblesApp/src/components/footer/Footer.jsx
+++ CollectiblesApp/src/components/footer/Footer.jsx
@@ -1,6 +1,17 @@
+import { Link } from 'react-router-dom'
+import styles from './Footer.module.scss'
+
 const Footer = () => {
     return <>
-        <p>Footer</p>
+        <footer>
+            <div className={styles.name}>
+                <p>By Khomenko Vitalii</p>
+            </div>
+            <div className={styles.links}>
+                <Link to="https://www.linkedin.com/in/vitalii-khomenko-860163255/"><img src='linkedin-icon.png' /></Link>
+                <Link to="https://github.com/KhomenkoVitalii/"><img src='github-icon.svg' /></Link>
+            </div>
+        </footer>
     </>
 }
 
