45940987ed94f98517bc89154408ac5959c25eab
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-19 00:20:03 +0200

    update AppRouter.jsx

--- CollectiblesApp/src/routes/AppRouter.jsx
+++ CollectiblesApp/src/routes/AppRouter.jsx
@@ -1,12 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../pages/App'
-import Home from '../pages/home/Home';
+import Home from '../pages/home/Home'
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
-        loader: rootLoader,
         children: [
             {
                 index: true,
