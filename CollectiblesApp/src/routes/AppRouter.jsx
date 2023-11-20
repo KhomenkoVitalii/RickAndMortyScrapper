90115165d80db68c93d8b62b75c21f62c09dbd26
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-20 14:38:18 +0200

    feat: implemented ErrorComponent

--- CollectiblesApp/src/routes/AppRouter.jsx
+++ CollectiblesApp/src/routes/AppRouter.jsx
@@ -2,11 +2,14 @@ import { createBrowserRouter } from 'react-router-dom'
 import App from '../pages/App'
 import Home from '../pages/home/Home'
 import Episode from '../pages/episodes/Episode';
+import ErrorComponent from '../components/error/ErrorComponent';
+
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        errorElement: <ErrorComponent />,
         children: [
             {
                 index: true,
