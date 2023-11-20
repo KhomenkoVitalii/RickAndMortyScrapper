938add13f7dcba562c60479b8d18ae192c6df1bd
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-20 18:20:29 +0200

    feat: created backend path dictionary

---
+++ CollectiblesApp/src/api/backend_path.js
@@ -0,0 +1,8 @@
+const baseUrl = "http://127.0.0.1:8000"
+
+const BackendPath = Object.freeze({
+    HOME: baseUrl + "/home/",
+    EPISODES_COUNT: baseUrl + "/api/seasons-list/count/"
+});
+
+export default BackendPath;
