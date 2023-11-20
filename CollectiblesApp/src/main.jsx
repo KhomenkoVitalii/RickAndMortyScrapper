10e8f0886e3dc4798f9f18198c521f86a4e6a791
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-20 14:08:42 +0200

    feat: removed react strict mode

--- CollectiblesApp/src/main.jsx
+++ CollectiblesApp/src/main.jsx
@@ -5,9 +5,9 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes/AppRouter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <RouterProvider router={router}>
       <App />
     </RouterProvider>
-  </React.StrictMode>,
+  </>,
 )
