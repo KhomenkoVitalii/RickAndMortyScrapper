ad7175013178a28e36fc1442317ae699b5a7972a
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-20 18:21:09 +0200

    feat: refactored home_request

--- CollectiblesApp/src/api/home_request.js
+++ CollectiblesApp/src/api/home_request.js
@@ -1,7 +1,7 @@
+import BackendPath from "./backend_path";
+
 const homeRequest = async () => {
-    const url = "http://127.0.0.1:8000/home/";
-    const response = await fetch(url);
-    return response;
+    return await fetch(BackendPath.HOME);
 }
 
 export default homeRequest;
