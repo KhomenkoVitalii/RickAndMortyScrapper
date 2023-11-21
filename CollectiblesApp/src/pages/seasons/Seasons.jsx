cd1bb9b84bd9a070d21baeee9425f5f1a0f0f0a5
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-21 18:28:24 +0200

    feat: Seasons component created

---
+++ CollectiblesApp/src/pages/seasons/Seasons.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import styles from './Seasons.module.scss';
+import { getSeasonsCountRequest, getSeasonEpisodesRequest } from '../../api/episode_requests';
+import SeasonComponent from '../../components/season/SeasonComponent';
+import { useSearchParams } from 'react-router-dom';
+import EpisodeItem from '../../components/episode/EpisodeItem';
+
+const Seasons = () => {
+    const [seasonsCount, setSeasonsCount] = useState(null);
+    const [seasonEpisodes, setSeasonEpisodes] = useState(null);
+    const [searchParams] = useSearchParams();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (searchParams.has('season')) {
+                    const response = await getSeasonEpisodesRequest(searchParams.get('season'));
+                    const data = await response.json();
+                    setSeasonEpisodes(data);
+                    console.log(data);
+                } else {
+                    const response = await getSeasonsCountRequest();
+                    const data = await response.json();
+                    const num = Number(data.season_number);
+                    setSeasonsCount(num);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, [searchParams]);
+
+
+    // get the 'season' parameter from the URL
+    const seasonParam = searchParams.get('season');
+
+    // If not exists, display seasons
+    if (!seasonParam) {
+        // If data loaded
+        if (seasonsCount) {
+            return (
+                <div className={styles.body}>
+                    {Array.from({ length: seasonsCount }, (_, index) => (
+                        <SeasonComponent key={index} number={index + 1} />
+                    ))}
+                </div>
+            );
+        }
+        // if not
+        return (
+            // TODO: Add loader or your existing content
+            <p>Loading...</p>
+        );
+    }
+
+    // If episodes exist, display episodes of the season
+    if (seasonEpisodes) {
+        return (
+            <>
+                <div className={styles.episodes}>
+                    {Array.from(seasonEpisodes, (episode, index) => (
+                        <EpisodeItem key={index} props={episode} />
+                    ))}
+                </div>
+            </>
+        );
+    } else {
+        // If episodes don't exist, display a loading message
+        return <p>Loading...</p>;
+    }
+};
+
+export default Seasons;
