146717b04046ce081e29689f3909e0d9d19ae5f4
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-23 19:18:11 +0200

    chore: decrease pagination to 24

--- RickAndMortyScrapper/settings.py
+++ RickAndMortyScrapper/settings.py
@@ -57,7 +57,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 42,
+    'PAGE_SIZE': 24,
 }
 
 MIDDLEWARE = [
