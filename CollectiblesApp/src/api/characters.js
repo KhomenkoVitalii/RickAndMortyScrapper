f34d6064bd6c61d8f020af82c000924276e961be
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-23 18:25:00 +0200

    feat: added fetch for searching

--- CollectiblesApp/src/api/characters.js
+++ CollectiblesApp/src/api/characters.js
@@ -1,8 +1,13 @@
 import BackendPath from "./backend_path"
 
 const getCharacters = (page) => {
-    const URL = BackendPath.CHARACTERS + `?page=${page}`
+    const URL = BackendPath.CHARACTERS + `?page=${page}`;
     return fetch(URL);
 }
 
-export { getCharacters };
+const searchForCharacters = (keyword) => {
+    const URL = BackendPath.CHARACTERS + `?search=${keyword}`;
+    return fetch(URL);
+}
+
+export { getCharacters, searchForCharacters };
