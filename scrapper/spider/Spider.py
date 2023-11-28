2fc32f3304a3258988a66a16c9d9d28298cc1f23
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-28 17:45:14 +0200

    feat: created parent class for Spiders

---
+++ scrapper/spider/Spider.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class Spider:
+    base_url = ""
+
+    def start_requests(self):
+        try:
+            response = requests.get(self.base_url)
+            response.raise_for_status()
+            self.parse(response.json())
+        except requests.exceptions.RequestException as e:
+            print(f"Error making initial request: {e}")
+
+    def parse(self):
+        raise NotImplemented
