ad5d9297f8b38b6046ee3d6137f60904c2b30c3d
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-28 18:03:01 +0200

    refactor episode_spider: increase code readability and safety

--- scrapper/spider/episode_spider.py
+++ scrapper/spider/episode_spider.py
@@ -1,54 +1,64 @@
 import requests
 from scrapper.models import Episode, Character
 from django.core.files.base import ContentFile
+from scrapper.spider import Spider
 
 
-class EpisodeSpider:
+class EpisodeSpider(Spider):
     base_url = 'https://rickandmortyapi.com/api/episode/'
 
-    def start_requests(self):
-        response = requests.get(self.base_url)
-        if response.status_code == 200:
-            self.parse(response.json())
-
-    def find_and_assign_characters_to_the_episode(self, episode, characters_urls):
+    def assign_characters_to_the_episode(self, episode, characters_urls):
         for character_url in characters_urls:
-            # make request
-            response = requests.get(character_url)
-            if response.status_code == 200:
-                # retrieve character object or create if it doesn't exist
-                character_data = response.json()
-                try:
-                    character = Character.objects.get(
-                        url=character_data.get('url'),
-                    )
-                except Character.DoesNotExist:
-                    print(f"Character {character_data['name']} doesn't exist!")
-                    continue
-                else:
-                    # add character to the episode
-                    episode.characters.add(character)
+            character_data = self.get_character(character_url)
+            try:
+                character = Character.objects.get(
+                    url=character_data.get('url'),
+                )
+            except Character.DoesNotExist:
+                print(f"Character {character_data['name']} doesn't exist!")
+                continue
+            else:
+                # add character to the episode
+                episode.characters.add(character)
+
+    def get_character(self, character_url):
+        # make request
+        response = requests.get(character_url)
+
+        if not response.status_code == 200:
+            print(
+                f"Error while fetching character: {response.status_code}")
+            raise response.raise_for_status()
+
+        # retrieve character object or create if it doesn't exist
+        return response.json()
+
+    def save_episode(self, episode_data):
+        # Create an instance of the model
+        episode_instance = Episode()
+
+        episode_instance.name = episode_data.get('name')
+        episode_instance.air_date = episode_data.get('air_date')
+        episode_instance.episode = episode_data.get('episode')
+        episode_instance.url = episode_data.get('url')
+        episode_instance.created = episode_data.get('created')
+
+        # Save the model instance to the database
+        episode_instance.save()
+
+        return episode_instance
 
     def parse(self, data):
         episodes = data['results']
 
         for episode_data in episodes:
-            # Create an instance of the model
-            episode_instance = Episode()
+            episode_instance = self.save_episode(episode_data)
 
-            episode_instance.name = episode_data.get('name')
-            episode_instance.air_date = episode_data.get('air_date')
-            episode_instance.episode = episode_data.get('episode')
-            episode_instance.url = episode_data.get('url')
-            episode_instance.created = episode_data.get('created')
-
-            # Save the model instance to the database
-            episode_instance.save()
             print(f"Created episode {episode_data['episode']}")
 
             characters_urls = episode_data['characters']
 
-            self.find_and_assign_characters_to_the_episode(
+            self.assign_characters_to_the_episode(
                 episode=episode_instance, characters_urls=characters_urls)
 
             print(
