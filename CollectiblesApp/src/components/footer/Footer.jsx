70f142f0d62c98813766b7cfcc5f8a5d97d20c42
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-11-28 12:37:57 +0200

    update Footer.jsx

--- CollectiblesApp/src/components/footer/Footer.jsx
+++ CollectiblesApp/src/components/footer/Footer.jsx
@@ -1,18 +1,23 @@
-import { Link } from 'react-router-dom'
-import styles from './Footer.module.scss'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Footer.module.scss';
 
 const Footer = () => {
-    return <>
-        <footer>
+    return (
+        <footer className={styles.footer}>
             <div className={styles.name}>
                 <p>By Khomenko Vitalii</p>
             </div>
             <div className={styles.links}>
-                <Link to="https://www.linkedin.com/in/vitalii-khomenko-860163255/"><img src='linkedin-icon.png' /></Link>
-                <Link to="https://github.com/KhomenkoVitalii/"><img src='github-icon.svg' /></Link>
+                <Link to="https://www.linkedin.com/in/vitalii-khomenko-860163255/" target="_blank" rel="noopener noreferrer">
+                    <img src='linkedin-icon.png' alt="LinkedIn Icon" />
+                </Link>
+                <Link to="https://github.com/KhomenkoVitalii/" target="_blank" rel="noopener noreferrer">
+                    <img src='github-icon.svg' alt="GitHub Icon" />
+                </Link>
             </div>
         </footer>
-    </>
+    );
 }
 
 export default Footer;
