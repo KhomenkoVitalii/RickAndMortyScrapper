1efa9f9f2d8714e96d9eb12c3e5cf878337640e8
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-12-01 15:27:54 +0200

    feat: implement AppContext into the App

--- CollectiblesApp/src/main.jsx
+++ CollectiblesApp/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './pages/App'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/AppRouter'
+import { AppContextProvider } from './context/AppContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <AppContextProvider>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </AppContextProvider>
   </>,
 )
