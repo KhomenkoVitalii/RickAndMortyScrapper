e180200db771295e90eb96dc6598aed5c08c576c
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-12-01 15:26:06 +0200

    feat AppRouter: create ProtectedRoute for my-collection
    If user is authorized it renders the component, if not redirect to login page

--- CollectiblesApp/src/routes/AppRouter.jsx
+++ CollectiblesApp/src/routes/AppRouter.jsx
@@ -1,11 +1,26 @@
-import { createBrowserRouter } from 'react-router-dom'
+import React from 'react';
+import { createBrowserRouter, useNavigate } from 'react-router-dom'
 import App from '../pages/App'
 import Home from '../pages/home/Home'
-import Episode from '../pages/episodes/Episode';
 import ErrorComponent from '../components/error/ErrorComponent';
 import Seasons from '../pages/seasons/Seasons';
 import Characters from '../pages/characters/Characters';
+import IsAuthorized from '../utils/IsAuthorized';
+import SignIn from '../components/signin/SignIn';
+import AuthPage from '../pages/auth/AuthPage';
+import MyCollection from '../pages/myCollection/MyCollection';
 
+
+const PrivateRoute = ({ component: Component, is_authorized, redirectPath, ...args }) => {
+    const navigate = useNavigate();
+    if (is_authorized) {
+        return <Component {...args} />;
+    } else {
+        console.log("User is not authorized!");
+        navigate(redirectPath);
+        return null;
+    }
+};
 
 const router = createBrowserRouter([
     {
@@ -18,6 +33,19 @@ const router = createBrowserRouter([
                 element: <Home />
             },
             {
+                path: '/login/',
+                element: <AuthPage />,
+                children: [
+                    {
+                        path: 'sign-in/',
+                        element: <SignIn />
+                    },
+                    {
+
+                    },
+                ],
+            },
+            {
                 path: '/seasons/',
                 element: <Seasons />,
             },
@@ -26,8 +54,12 @@ const router = createBrowserRouter([
                 element: <Characters />,
             },
             {
-                path: '/my-collections/',
-                element: <ErrorComponent />
+                path: '/my-collection/',
+                element: <PrivateRoute
+                    authorized={IsAuthorized}
+                    redirectPath="/login/sign-in/"
+                    component={MyCollection}
+                />
             }
         ],
     }
