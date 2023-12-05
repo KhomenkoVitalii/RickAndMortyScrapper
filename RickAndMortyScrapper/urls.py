07f51df1fe8a7a51a106316356b9cec2a8b609a8
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-12-05 14:34:29 +0200

    feat: implemented views changes

--- RickAndMortyScrapper/urls.py
+++ RickAndMortyScrapper/urls.py
@@ -1,29 +1,30 @@
 from django.contrib import admin
-from rest_framework import routers
-from django.urls import path, include
-from game.views import sign_in, sign_up, logout_request, home, UserCardView, AnotherUserCardsView
-from scrapper.views import LocationView, CharacterView, EpisodeView, SeasonEpisodeView
 from django.conf import settings
+from django.urls import path, include
 from django.conf.urls.static import static
+from rest_framework import routers
+from game import views
+from scrapper.views import LocationView, CharacterView, EpisodeView, SeasonEpisodeView
 
 urlpatterns = [
-    path('sign-up/', sign_up, name='register'),
-    path('sign-in/', sign_in, name='login'),
-    path('logout/', logout_request, name="logout"),
+    path('sign-up/', views.UserRegisterView.as_view(), name='register'),
+    path('sign-in/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name="logout"),
+    path('me/', views.UserView.as_view(), name='personal_data'),
     path('admin/', admin.site.urls),
-    path('home/', view=home, name='home'),
+    path('home/', view=views.HomeView.as_view(), name='home'),
     path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
 router = routers.DefaultRouter()
-router.register(r'api/usercard', UserCardView, basename='usercard')
+router.register(r'api/usercard', views.UserCardView, basename='usercard')
 router.register(r'api/location', LocationView, basename='location')
 router.register(r'api/character', CharacterView, basename='character')
 router.register(r'api/episode', EpisodeView, basename='episode')
 router.register(r'api/seasons-list', SeasonEpisodeView,
                 basename='seasons-list')
 router.register(
-    r'api/collection/(?P<user>[^/]+)', AnotherUserCardsView, basename='collection')
+    r'api/collection/(?P<user>[^/]+)', views.AnotherUserCardsView, basename='collection')
 
 
 urlpatterns += router.urls
