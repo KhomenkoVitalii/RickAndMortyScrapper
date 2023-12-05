a77977d7060bd4b0cfaa826f363ec02430455422
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2023-12-05 14:35:58 +0200

    feat: implemented changes to the location_spider
    Now it's creates custom url of our server not the third-party one

--- scrapper/spider/location_spider.py
+++ scrapper/spider/location_spider.py
@@ -1,15 +1,21 @@
 import requests
 from scrapper.serializer import LocationSerializer
 from scrapper.spider.spider import Spider
+from RickAndMortyScrapper.settings import PUBLIC_IP
 
 
 class LocationSpider(Spider):
     base_url = 'https://rickandmortyapi.com/api/location/'
+    URL = f"{PUBLIC_IP}/api/location/"
 
     def parse(self, data):
         locations = data['results']
 
         for location_data in locations:
+            # extract location id
+            location_id = location_data['id']
+            # set custom url
+            location_data['url'] = f"{self.URL}{location_id}"
             serializer = LocationSerializer(data=location_data)
             if serializer.is_valid():
                 location_instance = serializer.save()
